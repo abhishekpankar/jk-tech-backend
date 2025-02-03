35efcd5beb839da5b33c9d36fea9f96dce7af28c
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-03 11:37:19 +0530

    configs and validations added

---
+++ src/configurations/app.config.ts
@@ -0,0 +1,27 @@
+export const appConfig = () => ({
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiry: process.env.JWT_EXPIRY,
+  },
+  bcrypt: {
+    saltRounds: 10,
+  },
+  swagger: {
+    title: 'Wanna Blog',
+    version: '1.0',
+    path: 'api',
+  },
+  oAuth: {
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    redirectUri: process.env.OAUTH_REDIRECT_URI,
+  },
+  users: {
+    findAll: {
+      limit: 10,
+    },
+    defaultRoleId: 3,
+  },
+});
+
+export type AppConfig = ReturnType<typeof appConfig>;
