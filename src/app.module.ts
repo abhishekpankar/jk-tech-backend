786d2be32b54823ad9f07f72cd6567c63cdaf8b0
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-03 11:45:09 +0530

    missing modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { serverConfig } from './configurations/server.config';
+import { serverConfig, validate } from './configurations';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { appConfig } from './configurations/app.config';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['local.env', 'development.env', 'production.env', '.env'],
       isGlobal: true,
-      load: [serverConfig],
-      // cache: true,
-      // validate,
+      load: [serverConfig, appConfig],
+      cache: true,
+      validate,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true,
+        stopAtFirstError: true,
+        whitelist: true,
+        forbidUnknownValues: true,
+        forbidNonWhitelisted: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
