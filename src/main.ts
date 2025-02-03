2a341f3ee6ae3acfc9886c3a1658bb393bb09733
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-03 11:38:09 +0530

    swagger api docs added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ServerConfig } from './configurations/server.config';
 import { VersioningType } from '@nestjs/common';
+import { Environment } from './configurations';
+import { AppConfig } from './configurations/app.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +17,21 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // Get config
-  const config = app.get(ConfigService);
-  const { port } = config.get<ServerConfig>('config');
+  const configService = app.get(ConfigService);
+  const { port, nodeEnv } = configService.getOrThrow<ServerConfig>('config');
+
+  if (nodeEnv == Environment.Development) {
+    const swaggerConfig =
+      configService.getOrThrow<AppConfig['swagger']>('swagger');
+    const config = new DocumentBuilder()
+      .setTitle(swaggerConfig.title)
+      .setVersion(swaggerConfig.version)
+      .addBearerAuth()
+      .addSecurityRequirements('bearer')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(swaggerConfig.path, app, document);
+  }
 
   await app.listen(port);
 }
