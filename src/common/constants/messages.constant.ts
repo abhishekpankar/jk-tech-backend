0d143ed4e970847afb16905e904fd7e5adbf20b9
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2025-02-03 17:52:31 +0530

    message constants added

---
+++ src/common/constants/messages.constant.ts
@@ -0,0 +1,11 @@
+export const messagesConstant = {
+  LOGIN_RESPONSE: 'Login Successful',
+  USERS_FETCHED: 'Users Fetched',
+  USER_FETCHED: 'Users Fetched',
+  ADD_USER_RESPONSE: 'User Added',
+  INVALID_ROLE_ID: 'Invalid Role ID',
+  USER_ALREADY_EXISTS: 'User already exists',
+  USER_NOT_FOUND: 'User not found',
+  USER_DELETED: 'User Deleted',
+  USER_UPDATED: 'User Updated',
+};
